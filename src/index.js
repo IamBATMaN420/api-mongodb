cd9d40b4e07c92e042c3c19fa155e5946476ba69
helper.dev <basakayush942@gmail.com>
2025-06-08 16:29:35 +0530

    validation added

--- src/index.js
+++ src/index.js
@@ -26,9 +26,46 @@ mongoose.connection.on("error", (error) => {
 
 app.post("/api/products", async (req, res) => {
   try {
+
+    if (!req.body.name) {
+      return res.status(422).json({
+        "error": "name feild is not given"
+      })
+    }
+    if (!req.body.description) {
+      return res.status(422).json({
+        "error": "description feild is not given"
+      })
+    }
+    if (!req.body.price) {
+      return res.status(422).json({
+        "error": "price feild is not given"
+      })
+    }
+    if (!req.body.quantity) {
+      return res.status(422).json({
+        "error": "quantity feild is not given"
+      })
+    }
+    if (!req.body.active) {
+      return res.status(422).json({
+        "error": "active feild is not given"
+      })
+    }
+    if (!req.body.catagory) {
+      return res.status(422).json({
+        "error": "catagory feild is not given"
+      })
+    } else if (!product.schema.path("catagory").enumValues.includes(req.body.catagory)) {
+      return res.status(422).json({
+        "error": "catagory must be in the feild"
+      })
+    } 
     const product = await productsModels.create(req.body)
     res.status(201).json(product)
-  } catch (error) {
+  }
+
+  catch (error) {
     return res.status(501).json({
       "error": error.message
     })
